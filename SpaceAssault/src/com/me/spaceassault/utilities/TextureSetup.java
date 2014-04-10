68940646d09970e746d7c184d95fce0f95802634
Alberto Astiazaran <pmdmaster@hotmail.com>
2014-04-09 23:31:59 -0500

    ya están las texturaaaaaassss

--- SpaceAssault/src/com/me/spaceassault/utilities/TextureSetup.java
+++ SpaceAssault/src/com/me/spaceassault/utilities/TextureSetup.java
@@ -3,11 +3,12 @@
  */
 package com.me.spaceassault.utilities;
 
-import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+//import com.badlogic.gdx.tools.imagepacker.TexturePacker2;
 
 
 public class TextureSetup {
     public static void main (String[] args) throws Exception {
-        TexturePacker.process(inputDir, outputDir, packFileName);
+        //TexturePacker2.process("C:/Users/Alberto/git/AX3SpaceAssault/SpaceAssault-desktop/bin/images/characters/characterpng", 
+        //		"C:/Users/Alberto/git/AX3SpaceAssault/SpaceAssault-desktop/bin/images/characters/characterpng", "textures.pack");
     }
 }
